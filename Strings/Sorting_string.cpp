390f52a253a33d60c54ee69f511f9d87849dbf3c
MustafaAnsari-sd <mustafaansari45990@gmail.com>
2026-03-06 16:04:27 +0530

    anagram or not problem

---
+++ Strings/Sorting_string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    // string s;
+    // cin>>s;
+    // sort(s.begin(), s.end()); // sort based on ASCII values
+    // cout<<s;
+
+    // Anagram or not
+    // string s1;
+    // getline(cin,s1);
+    // string s2;
+    // getline(cin,s2);
+    // sort(s1.begin(), s1.end());
+    // sort(s2.begin(), s2.end());
+    // if(s1==s2){
+    //     cout<<"True";
+    // }
+    // else cout<<"false";
+
+    // Print the character occuring most
+    string s;
+    getline(cin,s);
+}
